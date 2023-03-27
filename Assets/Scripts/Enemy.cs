ec3e22564f71ee151f4a347a64d22d31b732051f
styleriven <duonghuy.180602@gmail.com>
2023-03-27 19:56:04 +0700

    Simple User Interface For Score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,27 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15f;
+    ScoreBoard scoreBoard;
 
-    void OnParticleCollision(GameObject other) 
+    void Start() {
+        scoreBoard = FindObjectOrType<ScoreBoard>();
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
