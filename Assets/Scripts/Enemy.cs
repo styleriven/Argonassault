c247eb3412874182c26854c4c7a7ed70e8c909c9
styleriven <duonghuy.180602@gmail.com>
2023-03-27 08:42:34 +0700

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
+    void OnParticleCollision(GameObject other) 
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
